17803ec32d90a72cfcf9b8d27139c41cc791a41a
Douglas Marra <douglasmsi@gmail.com>
2016-08-28 20:20:58 -0300

    Atualização

--- CasaDoCodigo/server.js
+++ CasaDoCodigo/server.js
@@ -1,4 +1,8 @@
 var app = require('./custom-express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.set('io',io);
 
 console.log('Tentando iniciar servidor...');
 app.listen(3000,function(){
